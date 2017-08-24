056b1d33f742c6c1cf4cc5d6b96f266ccddf6a6c
Rexhunt <rexoregan20@yahoo.com.au>
2017-08-24 19:29:02 +1000

    Keeps old DB if no update

--- Crypt-Money.py
+++ Crypt-Money.py
@@ -21,10 +21,12 @@ dbdatabase = "cryptovalue"
 #Connect to database specified above & create cursor for working with it.
 mariadb_connection = mariadb.connect(user = dbuser, password = dbpassword,
                                      host = dbhost, database = dbdatabase)
-cursor = mariadb_connection.cursor()
+cursor = mariadb_connection.cursor(buffered=True)
 
 #Specify the SQL statement to load data into the database
 dbinsert = "INSERT INTO Table3 (curid,price_btc,last_updated) VALUES (%s,%s,%s)"
+#Specify the SQL query to check if we have more recent data
+dblatest = "SELECT MAX(last_updated) FROM Table3 WHERE curid = (%s)"
 
 
 #Loop through all currencies from the JSON
@@ -32,9 +34,23 @@ count=0
 while count < len(data):
     curid = data[count]["id"]
     price_btc = data[count]["price_btc"]
-    last_updated = datetime.datetime.fromtimestamp(int(
-        data[count]["last_updated"]))
-    cursor.execute(dbinsert,(curid, price_btc, last_updated))
+    last_updated = str(datetime.datetime.fromtimestamp(int(
+        data[count]["last_updated"])))
+    #Check to see if we actually need to update the DB
+    cursor.execute(dblatest,(curid,))
+    result = str(cursor.fetchone())
+    if result != "(None,)":
+        db_date = datetime.datetime.strptime(result,
+                                         "(datetime.datetime(%Y, %m, %d, %H, %M, %S),)")
+    else:
+        db_date = 0
+    last_updated = datetime.datetime.strptime(last_updated,
+                                         "%Y-%m-%d %H:%M:%S")
+    if last_updated == db_date:
+        print("Timestamps are =, keeping old DB values")
+    else:
+        print("Timestamps are <> Adding to DB.")
+        cursor.execute(dbinsert,(curid, price_btc, last_updated))
     count = count + 1
 
 mariadb_connection.commit()
